370a3077ab4f5fd85bea269c0fb0e7936cd0abfe
hyegineer <hyegineer@gmail.com>
2022-01-03 16:00:10 +0900

    calendar 짜는중...

--- step-06/calendar/index.js
+++ step-06/calendar/index.js
@@ -1,43 +1,53 @@
-const today = new Date();
 const weekly = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+const daysWrap = document.querySelector("#days-wrap");
+const day = document.querySelector("#day");
+
+const today = new Date();
+let todayYear = today.getFullYear();
+let todayDate = today.getDate();
+let todayMonth = today.getMonth();
+let todayDay = today.getDay();
 
 /* ==================================================================== */
 
-// 오늘의 현재 요일 표기
-const day = today.getDay();
-// 오늘의 현재 날짜 표기
-const date = today.getDate();
-// 오늘의 현재 월 표기
-const month = today.getMonth();
-// 오늘의 현재 연도 표기
-const year = today.getFullYear();
+loadCalendar(todayYear, todayMonth);
 
-/* ==================================================================== */
+function loadCalendar(y, m){
+  const first = new Date(y, m, 1).getDay();
+  const last = new Date(y, m + 1, 0).getDate();
+  const total = first + last;
 
-const firstOfMonth = new Date(year, month, 1).getDay();
-const lastOfMonth = new Date(year, month + 1, 0).getDate();
-const totalOfMonth = firstOfMonth + lastOfMonth;
-
-// 날짜 칸 만들기
-for (let i = 0; i < totalOfMonth; i++) {
-  const div = document.createElement("div");
-  div.classList.add("cell");
-  if (i === date + firstOfMonth - 1) {
-    div.style.color = "red";
-  }
-  document.querySelector("#days-wrap").appendChild(div);
-}
-
-let cells = document.querySelectorAll("#days-wrap .cell");
-
-for (let j = 0; j < totalOfMonth; j++) {
-  if (firstOfMonth + j === totalOfMonth) {
-    break;
+  // 칸 생성
+  while(daysWrap.hasChildNodes()){
+    daysWrap.removeChild(daysWrap.firstChild);
   }
 
-  cells[firstOfMonth + j].textContent = j + 1;
+  for (let i = 0; i < total; i++) {
+    const div = document.createElement("div");
+    div.classList.add("cell");
+    daysWrap.appendChild(div);
+  }
+
+  let cells = document.querySelectorAll("#days-wrap .cell");
+  
+  // 칸에 날짜 삽입
+  for (let j = 0; j < total; j++) {
+    if (first + j === total) {
+      break;
+    }
+  
+    cells[first + j].textContent = j + 1;
+  }
 }
 
 // // 우측 화살표를 클릭 했을때, 다음 달의 요일 및 날짜 표기
+document.querySelector('.btn-arrow-next').addEventListener('click', function(){
+  loadCalendar(year, ++month)
+})
+
 // // 좌측 화살표를 클릭 했을때, 이전 달의 요일 및 날짜 표기
+document.querySelector('.btn-arrow-prev').addEventListener('click', function(){
+  loadCalendar(year, --month)
+})
+
 // // 특정 날짜를 클릭 했을때, 상단의 요일 및 날짜 반영하기
